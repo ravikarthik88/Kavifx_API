5c458f8d8458a05d7318bdad35084c4304b7083a
ravikarthik88 <32273326+ravikarthik88@users.noreply.github.com>
2024-02-06 22:58:33 +0530

    Basic Settings Before API Creation

---
+++ Models/KavifxDbContext.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Security;
+
+namespace Kavifx_API.Models
+{
+    public class KavifxDbContext : DbContext
+    {
+        public KavifxDbContext(DbContextOptions<KavifxDbContext> opts):base(opts) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserProfile> Profiles { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+    }
+
+
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } = "User";
+    }
+
+    public class UserProfile
+    {
+        [Key]
+        public int UserProfileId { get; set; }        
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public string ProfilePictureUrl { get; set; }
+    }
+
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+
+    public class UserRole
+    {
+        [Key]
+        public int UserRoleId { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public int RoleId { get; set; }
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+    }
+
+    public class Permission
+    {
+        [Key]
+        public int PermissionId { get; set; }
+        public string PermissionName { get; set; }
+    }
+
+    public class RolePermission
+    {
+        [Key]
+        public int RolePermissionId { get; set; }
+        public int RoleId { get; set; }
+        public int PermissionId { get; set; }
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+        [ForeignKey("PermissionId")]
+        public Permission Permission { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,66 @@
+using Kavifx_API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<KavifxDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//Enable Cors Policy
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("CrossPolicy", copl =>
+    {
+        copl.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+
+//Enable JWT Authentication
+byte[] keybytes = System.Text.Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWTDATA:Key").Value);
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opts =>
+{
+    opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        IssuerSigningKey = new SymmetricSecurityKey(keybytes),
+        ValidIssuer = builder.Configuration.GetSection("JWTDATA:Issuer").Value,
+        ValidAudience = builder.Configuration.GetSection("JWTDATA:Issuer").Value
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authentication Header using the Bearer Scheme(\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
 
+    c.OperationFilter<SecurityRequirementsOperationFilter>();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Kavifx_API", Version = "v1" });
+});
 var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -18,8 +71,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
+app.UseCors();
+
 app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
 
 app.Run();
